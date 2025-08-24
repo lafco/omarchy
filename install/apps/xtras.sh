767cd72763e132991c71af94243af8da2796e779
lafco <vinigfalconi@outlook.com.br>
2025-08-24 17:53:50 -0300

    Webapps: default packages

--- install/apps/xtras.sh
+++ install/apps/xtras.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-if [ -z "$OMARCHY_BARE" ]; then
-  yay -S --noconfirm --needed \
-    gnome-calculator gnome-keyring signal-desktop \
-    obsidian-bin libreoffice obs-studio kdenlive \
-    xournalpp localsend-bin
-
-  # Packages known to be flaky or having key signing issues are run one-by-one
-  for pkg in pinta typora spotify zoom; do
-    yay -S --noconfirm --needed "$pkg" ||
-      echo -e "\e[31mFailed to install $pkg. Continuing without!\e[0m"
-  done
-
-  yay -S --noconfirm --needed 1password-beta 1password-cli ||
-    echo -e "\e[31mFailed to install 1password. Continuing without!\e[0m"
-fi
-
 yay -S --noconfirm --needed xournalpp spotify capacities-appimage
 # Copy over Omarchy applications
 source omarchy-refresh-applications || true
