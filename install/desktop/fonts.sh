7a1f978537272bb54afd72278d4e5e840d56c384
lafco <vinigfalconi@outlook.com.br>
2025-08-24 17:57:30 -0300

    Fix: remove apple stuff

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts #noto-fonts-emoji
-
-# if [ -z "$OMARCHY_BARE" ]; then
-#   yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
-# fi
-
+yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
 yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
