2b1911b98fd067a652629eacfc74c1d931aad04e
lafco <vinigfalconi@outlook.com.br>
2025-08-24 12:29:18 -0300

    Packages: add firefox yazi nvm

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -6,7 +6,8 @@ yay -S --noconfirm --needed \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
   slurp satty \
   mpv evince imv \
-  chromium
+  chromium \
+  firefox yazi
 
 # Add screen recorder based on GPU
 if lspci | grep -qi 'nvidia'; then
